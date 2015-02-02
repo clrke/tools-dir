04f21dce426892349a57164d85536f025b95e865
Maverick Parane <mavericklozadaparane@gmail.com>
2015-02-02 19:54:26 +0800

    Add "Mavs" to local ENV

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('arkeidolon-HP-G42-Notebook-PC'),
+	'local' => array('arkeidolon-HP-G42-Notebook-PC', 'Mavs'),
 
 ));
 
