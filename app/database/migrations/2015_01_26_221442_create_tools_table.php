5648ebc48b2a8659d2427790c0aba67a3c92d067
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-02-02 20:24:39 +0800

    Fix migration class of tools table

--- app/database/migrations/2015_01_26_221442_create_tools_table.php
+++ app/database/migrations/2015_01_26_221442_create_tools_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePapersTable extends Migration {
+class CreateToolsTable extends Migration {
 	/**
 	 * Run the migrations.
 	 *
