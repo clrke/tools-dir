80aee17d71ca371c9a2e9a996718dedb46c818a4
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-02-07 16:30:52 +0800

    Set title to Tools Directory

--- app/views/papers/index.blade.php
+++ app/views/papers/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE>
 <html ng-app>
 	<head>
-		<title>Thesis Papers</title>
+		<title>Tools Directory</title>
 		<link rel="stylesheet" type="text/css" href="foundation/css/foundation.min.css">
 		<link rel="stylesheet" type="text/css" href="src/css/main.css">
 		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
