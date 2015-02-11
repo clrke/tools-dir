6f1107bdab8e4eac937fa81823a45d970d848bb9
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-02-11 15:26:22 +0800

    Add voters relationship for Tool model

--- app/models/Tool.php
+++ app/models/Tool.php
@@ -2,4 +2,9 @@
 
 class Tool extends Eloquent {
 	protected $fillable = ['title', 'abstract', 'authors', 'pageCount', 'year'];
+
+	public function voters()
+	{
+		return $this->belongsToMany('User', 'votes')->withPivot('is_positive');
+	}
 }
