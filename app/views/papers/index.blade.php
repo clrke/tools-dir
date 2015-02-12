2fdaffa4a869e48c9ccd54d3774fff1fc27bbae3
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-02-12 09:18:49 +0800

    Add foundicons-social

--- app/views/papers/index.blade.php
+++ app/views/papers/index.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Tools Directory</title>
 		<link rel="stylesheet" type="text/css" href="foundation/css/foundation.min.css">
+		<link rel="stylesheet" type="text/css" href="foundation/foundation_icons_social/stylesheets/social_foundicons.css">
 		<link rel="stylesheet" type="text/css" href="src/css/main.css">
 		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 		<script type="text/javascript" src="foundation/js/foundation.min.js"></script>
