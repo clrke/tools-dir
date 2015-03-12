fb6679ae2d8f924bd8fc2c8597c99920f90183a7
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2015-03-13 06:11:51 +0800

    Update to pretty-lists 1.2.0

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -3112,34 +3112,49 @@ function ref(obj, str) {
 }
 
 module.exports = {
-	format1: function (items, displayAttr) {
-		if(items.length === 0) {
-			return "0";
-		} else if(items.length == 1) {
-			return items[0][displayAttr];
-		} else if(items.length == 2) {
-			return items[0][displayAttr] +
-				" and " + items[1][displayAttr];
-		} else if(items.length == 3) {
-			return items[0][displayAttr] +
-				", " + items[1][displayAttr] +
-				" and another";
-		} else {
-			return items[0][displayAttr] +
-				", " + items[1][displayAttr] +
-				" and " + (items.length - 2) + " others";
-		}
-	},
-	format2: function (items, displayAttr1, displayAttr2) {
+	getItemsDisplay: function (items, displayAttr1, displayAttr2) {
 		items2 = [];
 
 		items.forEach(function (item) {
-			items2.push({
-				value: ref(item, displayAttr1) + " (" + ref(item, displayAttr2) + ")"
-			});
+			if(displayAttr2) {
+				items2.push({
+					value: ref(item, displayAttr1) +
+						" (" + ref(item, displayAttr2) + ")"
+				});
+			} else {
+				items2.push({
+					value: ref(item, displayAttr1)
+				});
+			}
 		});
 
-		return this.format1(items2, 'value');
+		return items2;
+	},
+	format1: function (items, displayAttr) {
+		var items2 = this.getItemsDisplay(items, displayAttr);
+
+		if(items.length === 0) {
+			return "0";
+		} else if(items.length == 1) {
+			return items2[0].value;
+		} else if(items2.length == 2) {
+			return items2[0].value +
+				" and " + items2[1].value;
+		} else if(items2.length == 3) {
+			return items2[0].value +
+				", " + items2[1].value +
+				" and another";
+		} else {
+			return items2[0].value +
+				", " + items2[1].value +
+				" and " + (items2.length - 2) + " others";
+		}
+	},
+	format2: function (items, displayAttr1, displayAttr2) {
+		return this.format1(
+			this.getItemsDisplay(
+				items, displayAttr1, displayAttr2
+			), 'value');
 	}
 }
 
